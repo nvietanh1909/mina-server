d3671712b80bac307bed75f1fa80dab8abd15bac
nvietanh1909 <nvietanh.190904@example.com>
2025-02-16 20:49:20 +0700

    + Hotfix

--- server.js
+++ server.js
@@ -39,5 +39,4 @@ app.use('/api/bills', billScanRoutes);
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Local: http://localhost:${PORT}`);
-  console.log(`Network: http://${IP}:${PORT}`);
 });
